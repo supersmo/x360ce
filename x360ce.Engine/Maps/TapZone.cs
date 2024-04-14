fe7b75b017809085204a405093d54c8297b7c561
supersmo <supersmo@users.noreply.github.com>
2024-04-15 01:18:35 +0200

    TapZone WIP

---
+++ x360ce.Engine/Maps/TapZone.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace x360ce.Engine.Maps
+{
+    public class TapZone
+    {
+        public TapZone(float tapValue, short minCycles=1)
+        {
+            this.tapValue = tapValue;
+            this.minCycles = minCycles;
+        }
+        private float tapValue;
+        private short cycle = 0;
+        private short minCycles;
+        private float cyclesPressed = 0f;
+        private float cyclesReleased = 0f;
+        private bool isPressed = false;
+
+        public float GetOutput(float targetValue)
+        {
+            //TODO: Handle transition from positive to negative target
+
+            cycle++;
+            float average = 0;
+            float cyclesTotal = cyclesPressed + cyclesReleased;
+
+            //calculate current average
+            if (cyclesTotal > 0) 
+            {
+                average = cyclesPressed * tapValue / cyclesTotal;
+            }
+
+            int targetSign = Math.Sign(targetValue);
+            float absTargetValue = Math.Abs(targetValue);
+
+            bool needToPressLonger = isPressed && cyclesPressed < minCycles;
+            bool needToReleaseLonger = !isPressed && cyclesReleased < minCycles && cycle != 1;
+            if ( needToPressLonger || !needToReleaseLonger &&  absTargetValue > 0 && average <= absTargetValue)
+            {
+                //Press
+                if (!isPressed) {
+                    isPressed = true;
+                    if (absTargetValue > 0) {
+                        float optimalReleaseCycles = (cyclesPressed * (tapValue - absTargetValue)) / absTargetValue;
+                        cyclesReleased = cyclesReleased - optimalReleaseCycles;
+                        cyclesPressed = 0;
+                    }
+                }
+                
+                cyclesPressed += 1;
+                return tapValue * targetSign;
+            } else {
+                //Release
+                if(isPressed)
+                {
+                    isPressed = false;
+                    if (tapValue > absTargetValue)
+                    { 
+                        float optimalPressCycles = (absTargetValue * cyclesReleased/(tapValue - absTargetValue));
+                        cyclesPressed = cyclesPressed - optimalPressCycles;
+                        cyclesReleased = 0;
+                    }
+                }
+
+                cyclesReleased += 1;
+                return 0;
+            }
+        }
+    }
+}
--- x360ce.Net48Test/Common/x360ceEngineTest.cs
+++ x360ce.Net48Test/Common/x360ceEngineTest.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using x360ce.App.Controls;
 using x360ce.Engine.Data;
+using x360ce.Engine.Maps;
 
 namespace x360ce.Tests
 {
@@ -28,4 +29,154 @@ namespace x360ce.Tests
 		}
 
 	}
+
+	[TestClass]
+	public class TapZoneTest
+	{
+		[TestMethod]
+		public void TestTapTargetZero()
+		{
+			TapZone tapZone = new TapZone(1);
+            Assert.AreEqual(0, tapZone.GetOutput(0));
+            Assert.AreEqual(0, tapZone.GetOutput(0));
+            Assert.AreEqual(0, tapZone.GetOutput(0));
+            Assert.AreEqual(0, tapZone.GetOutput(0));
+        }
+
+        [TestMethod]
+		public void TestTapTarget50Percent()
+		{
+            TapZone tapZone = new TapZone(1);
+            Assert.AreEqual(1, tapZone.GetOutput(0.5f));
+            Assert.AreEqual(0, tapZone.GetOutput(0.5f));
+            Assert.AreEqual(1, tapZone.GetOutput(0.5f));
+            Assert.AreEqual(0, tapZone.GetOutput(0.5f));
+            Assert.AreEqual(1, tapZone.GetOutput(0.5f));
+            Assert.AreEqual(0, tapZone.GetOutput(0.5f));
+        }
+
+		[TestMethod]
+        public void TestTapTarget25Percent()
+        {
+            TapZone tapZone = new TapZone(1);
+            Assert.AreEqual(1, tapZone.GetOutput(0.25f));//100%
+            Assert.AreEqual(0, tapZone.GetOutput(0.25f));// 50%
+            Assert.AreEqual(0, tapZone.GetOutput(0.25f));// 33%
+            Assert.AreEqual(0, tapZone.GetOutput(0.25f));// 25%
+
+            Assert.AreEqual(1, tapZone.GetOutput(0.25f));//100%
+            Assert.AreEqual(0, tapZone.GetOutput(0.25f));// 50%
+            Assert.AreEqual(0, tapZone.GetOutput(0.25f));// 33%
+            Assert.AreEqual(0, tapZone.GetOutput(0.25f));// 25%
+
+            Assert.AreEqual(1, tapZone.GetOutput(0.25f));//100%
+        }
+
+        [TestMethod]
+        public void TestTapTarget40Percent()
+        {
+            TapZone tapZone = new TapZone(1);
+            Assert.AreEqual(1, tapZone.GetOutput(0.4f));//100%
+            Assert.AreEqual(0, tapZone.GetOutput(0.4f));// 50%
+            Assert.AreEqual(0, tapZone.GetOutput(0.4f));// 33%
+            Assert.AreEqual(1, tapZone.GetOutput(0.4f));// 50%
+            Assert.AreEqual(0, tapZone.GetOutput(0.4f));// 40%
+
+            Assert.AreEqual(1, tapZone.GetOutput(0.4f));//100%
+            Assert.AreEqual(0, tapZone.GetOutput(0.4f));// 50%
+            Assert.AreEqual(0, tapZone.GetOutput(0.4f));// 33%
+            Assert.AreEqual(1, tapZone.GetOutput(0.4f));// 50%
+            Assert.AreEqual(0, tapZone.GetOutput(0.4f));// 40%
+        }
+
+        [TestMethod]
+        public void TestTapTarget75Percent()
+        {
+            TapZone tapZone = new TapZone(1);
+            Assert.AreEqual(1, tapZone.GetOutput(0.75f));//100%
+            Assert.AreEqual(0, tapZone.GetOutput(0.75f));// 50%
+            Assert.AreEqual(1, tapZone.GetOutput(0.75f));// 67%
+            Assert.AreEqual(1, tapZone.GetOutput(0.75f));// 75%
+
+            Assert.AreEqual(0, tapZone.GetOutput(0.75f));// 60% release due to rounding
+            Assert.AreEqual(1, tapZone.GetOutput(0.75f));// 66%
+            Assert.AreEqual(1, tapZone.GetOutput(0.75f));// 71%
+            Assert.AreEqual(1, tapZone.GetOutput(0.75f));// 75%
+
+            Assert.AreEqual(1, tapZone.GetOutput(0.75f));// 78%
+            Assert.AreEqual(0, tapZone.GetOutput(0.75f));// 70%
+            Assert.AreEqual(1, tapZone.GetOutput(0.75f));// 73%
+            Assert.AreEqual(1, tapZone.GetOutput(0.75f));// 75%
+        }
+
+        [TestMethod]
+        public void TestTapTarget100Percent()
+        {
+            TapZone tapZone = new TapZone(1);
+            Assert.AreEqual(1, tapZone.GetOutput(1));
+            Assert.AreEqual(1, tapZone.GetOutput(1));
+            Assert.AreEqual(1, tapZone.GetOutput(1));
+            Assert.AreEqual(1, tapZone.GetOutput(1));
+        }
+
+        [TestMethod]
+        public void TestTapTarget25PercentWithTapValue50Percent()
+        {
+            TapZone tapZone = new TapZone(0.5f);
+            Assert.AreEqual(0.5, tapZone.GetOutput(0.25f));//50%
+            Assert.AreEqual(0, tapZone.GetOutput(0.25f));  //25%
+            Assert.AreEqual(0.5, tapZone.GetOutput(0.25f));//33%
+            Assert.AreEqual(0, tapZone.GetOutput(0.25f));  //25%
+            Assert.AreEqual(0.5, tapZone.GetOutput(0.25f));//30%
+            Assert.AreEqual(0, tapZone.GetOutput(0.25f));  //25%
+            Assert.AreEqual(0.5, tapZone.GetOutput(0.25f));//29%
+            Assert.AreEqual(0, tapZone.GetOutput(0.25f));  //25%
+            Assert.AreEqual(0.5, tapZone.GetOutput(0.25f));//28%
+        }
+
+        [TestMethod]
+        public void TestTapTargetMinus25Percent()
+        {
+            TapZone tapZone = new TapZone(1);
+            Assert.AreEqual(-1, tapZone.GetOutput(-0.5f));
+            Assert.AreEqual(0, tapZone.GetOutput(-0.5f));
+            Assert.AreEqual(-1, tapZone.GetOutput(-0.5f));
+            Assert.AreEqual(0, tapZone.GetOutput(-0.5f));
+            Assert.AreEqual(-1, tapZone.GetOutput(-0.5f));     
+        }
+
+        [TestMethod]
+        public void TestTapTarget40PercentWithMinCycles2()
+        {
+            TapZone tapZone = new TapZone(1, 2);
+            Assert.AreEqual(1, tapZone.GetOutput(0.4f));//100%
+            Assert.AreEqual(1, tapZone.GetOutput(0.4f));//100% <-- Extra press cycle becaues of minCycles
+            Assert.AreEqual(0, tapZone.GetOutput(0.4f));// 66%
+            Assert.AreEqual(0, tapZone.GetOutput(0.4f));// 50%
+            Assert.AreEqual(0, tapZone.GetOutput(0.4f));// 40%
+
+            Assert.AreEqual(1, tapZone.GetOutput(0.4f));//100%
+            Assert.AreEqual(1, tapZone.GetOutput(0.4f));// 50% <-- Extra press cycle becaues of minCycles
+            Assert.AreEqual(0, tapZone.GetOutput(0.4f));// 33%
+            Assert.AreEqual(0, tapZone.GetOutput(0.4f));// 50%
+            Assert.AreEqual(0, tapZone.GetOutput(0.4f));// 40%
+        }
+
+        [TestMethod]
+        public void TestTapTarget60PercentWithMinCycles2()
+        {
+            TapZone tapZone = new TapZone(1, 2);
+            Assert.AreEqual(1, tapZone.GetOutput(0.6f));//100%
+            Assert.AreEqual(1, tapZone.GetOutput(0.6f));//100% <-- Extra press cycle Becaues of minCycles
+            Assert.AreEqual(0, tapZone.GetOutput(0.6f));// 66%
+            Assert.AreEqual(0, tapZone.GetOutput(0.6f));// 50%
+            Assert.AreEqual(1, tapZone.GetOutput(0.6f));// 60%
+
+            Assert.AreEqual(1, tapZone.GetOutput(0.6f));//67%
+            Assert.AreEqual(0, tapZone.GetOutput(0.6f));// 57%
+            Assert.AreEqual(0, tapZone.GetOutput(0.6f));// 50% <-- Extra release cycle becaues of minCycles
+            Assert.AreEqual(1, tapZone.GetOutput(0.6f));// 56%
+            Assert.AreEqual(1, tapZone.GetOutput(0.6f));// 60%
+        }
+    }
 }
